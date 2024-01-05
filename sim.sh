bac1dd996d58034f13e18e50852917af88c20aee
lukas <luk88luk@gmail.com>
2024-01-05 11:28:30 +0100

    now all changes are implemented

--- sim.sh
+++ sim.sh
@@ -1,4 +1,4 @@
 python3  scripts/hex_generator.py scripts/opcode_list.txt scripts/instruction_list.txt  hex.hex
 rm sim sim.vcd
-iverilog -g2009 -Wall -s crtl_mod_tb2 -o sim control_module.sv crtl_mod_tb2.sv decoder.sv linked_reg.sv memory.sv parameters.h pc2.sv instructions.sv 
+iverilog -g2009 -Wall -s crtl_mod_tb2 -o sim control_module.sv crtl_mod_tb2.sv decoder.sv linked_reg.sv memory.sv parameters.h pcs/pc2.sv instructions.sv 
 vvp sim 
